a76046b7147ebe8347b2e65c5b66a1ef16e87b9f
tirtha18 <tirthamunai1234@gmail.com>
2024-06-15 04:37:43 +0530

    fixed bugs in backend and frontend

--- Backend/controllers/togglestatus.js
+++ Backend/controllers/togglestatus.js
@@ -1,4 +1,3 @@
-import mongoose from "mongoose";
 import Sheets from "../models/Sheet.js";
 
 export const togglestatus = async (req, res) => {
@@ -9,6 +8,7 @@ export const togglestatus = async (req, res) => {
     let flag = false;
     const sheets = await Sheets.findById(sheets_id);
     let updated_sheets = sheets.sheets;
+    console.log(updated_sheets[2]._id);
     for (let i = 0; i < updated_sheets.length; i++) {
       if (updated_sheets[i]._id.toString() === sheet_id) {
         for (let j = 0; j < updated_sheets[i].sheet.length; j++) {
@@ -17,30 +17,32 @@ export const togglestatus = async (req, res) => {
             if (updated_sheets[i].sheet[j].status == "PENDING")
               updated_sheets[i].sheet[j].status = "DONE";
             else updated_sheets[i].sheet[j].status = "PENDING";
-            break;
           }
+          if (flag) break;
         }
       }
-      if (flag) {
-        Sheets.findByIdAndUpdate(sheets_id, {
-          $set: { sheets: updated_sheets },
-        })
-          .then((problem) => {
-            {
-              if (!problem)
-                return res.json({ message: "Problem status toggled" });
-            }
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      } else {
-        return res
-          .status(400)
-          .json({ messaga: "Invalid sheet,problem or sheet ids" });
-      }
     }
-    return res.json({ message: "Problem status updated" });
+    console.log(flag);
+    if (flag) {
+      try {
+        const getupsheed = async () => {
+          const updSheet = await Sheets.findByIdAndUpdate(sheets_id, {
+            $set: { sheets: updated_sheets },
+          });
+          res.json({
+            message: "Problem status toggled successfully",
+            updSheet,
+          });
+        };
+        getupsheed();
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      return res
+        .status(400)
+        .json({ messaga: "Invalid sheet, problem or sheet ids" });
+    }
   } catch (error) {
     console.log(error);
   }
--- Frontend/src/components/Sheets.jsx
+++ Frontend/src/components/Sheets.jsx
@@ -8,7 +8,7 @@ import { AuthContext } from "./AuthProvider";
 import { FileUploader } from "react-drag-drop-files";
 import { IoMdArrowRoundBack } from "react-icons/io";
 
-function FileUploadForm({ setFileshow, user_id }) {
+function FileUploadForm({ setFileshow, user_id, setReload }) {
   const [selectedFile, setSelectedFile] = useState(null);
   const handleFileChange = (file) => {
     setSelectedFile(file);
@@ -32,6 +32,7 @@ function FileUploadForm({ setFileshow, user_id }) {
         }
       );
       console.log(response);
+      setReload(true);
     } catch (error) {
       console.log(error);
     }
@@ -50,6 +51,8 @@ function FileUploadForm({ setFileshow, user_id }) {
           onClick={() => {
             setFileshow(false);
             FileUpload();
+            if (!selectedFile)
+              return alert("Select or drop a file to continue");
           }}
           disabled={!selectedFile}
         >
@@ -68,14 +71,48 @@ function FileUploadForm({ setFileshow, user_id }) {
     </div>
   );
 }
-function Sheet({ setSheetshow, selectedsheet }) {
+
+function Sheet({
+  setSheetshow,
+  selectedsheet,
+  sheet_id,
+  sheets_id,
+  setReload,
+}) {
+  const [problem_id, setProblem_id] = useState(null);
+
+  useEffect(() => {
+    const toggleProblemstatus = async () => {
+      console.log(sheet_id, "/n", sheets_id, "/n", problem_id);
+      if (problem_id && sheet_id && sheets_id) {
+        try {
+          console.log(1);
+          const response = await axios.post(
+            "https://code-xl.onrender.com/api/toggleprobstatus",
+            { problem_id: problem_id, sheets_id: sheets_id, sheet_id: sheet_id }
+          );
+          console.log(1);
+          console.log(response);
+          setReload(true);
+          setProblem_id("");
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    };
+    toggleProblemstatus();
+  }, [problem_id]);
+
+  const handleStatusToggle = (problem_id) => {
+    setProblem_id(problem_id);
+  };
+
   return (
-    <div className="p-4 capitalize">
+    <div className="p-4 capitalize m-6">
       <button
-        className="px-4 py-2 rounded-lg text-black bg-white mb-8 font-bold flex flex-row items-center space-x-1 "
+        className="px-4 py-2 rounded-lg text-black bg-white mb-8 font-bold flex flex-row items-center space-x-1"
         onClick={() => {
           setSheetshow(false);
-          console.log(selectedsheet);
         }}
       >
         <IoMdArrowRoundBack size={20} />
@@ -85,6 +122,9 @@ function Sheet({ setSheetshow, selectedsheet }) {
         <table className="w-full bg-gray-800 text-white">
           <thead>
             <tr>
+              <th className="py-2 px-4 border-b border-gray-700 bg-gray-900 text-left">
+                Status
+              </th>
               <th className="py-2 px-4 border-b border-gray-700 bg-gray-900 text-left">
                 Topic Name
               </th>
@@ -96,9 +136,6 @@ function Sheet({ setSheetshow, selectedsheet }) {
               </th>
               <th className="py-2 px-4 border-b border-gray-700 bg-gray-900 text-left">
                 Video
-              </th>
-              <th className="py-2 px-4 border-b border-gray-700 bg-gray-900 text-left">
-                Status
               </th>
             </tr>
           </thead>
@@ -112,6 +149,23 @@ function Sheet({ setSheetshow, selectedsheet }) {
                     : "bg-gray-800 hover:bg-gray-700"
                 }
               >
+                <td className="py-2 px-4 border-b border-gray-700">
+                  <button
+                    onClick={() => {
+                      handleStatusToggle(problem._id);
+                    }}
+                  >
+                    {problem.status === "DONE" ? (
+                      <div className="  font-semibold rounded-2xl px-2 flex flex-auto items-center justify-center bg-green-800 text text-green-400">
+                        Done
+                      </div>
+                    ) : (
+                      <div className="  font-semibold rounded-2xl px-2 flex flex-auto items-center justify-center bg-red-800 text text-red-400">
+                        Pending
+                      </div>
+                    )}
+                  </button>
+                </td>
                 <td className="py-2 px-4 border-b border-gray-700">
                   {problem.tag}
                 </td>
@@ -138,14 +192,6 @@ function Sheet({ setSheetshow, selectedsheet }) {
                     Video Link
                   </a>
                 </td>
-                <td className="py-2 px-8 border-b border-gray-700">
-                  <input
-                    type="checkbox"
-                    checked={problem.done || false}
-                    onChange={() => handleCheckboxChange(index)}
-                    className="mr-2"
-                  />
-                </td>
               </tr>
             ))}
           </tbody>
@@ -154,6 +200,7 @@ function Sheet({ setSheetshow, selectedsheet }) {
     </div>
   );
 }
+
 export default function Sheets() {
   const [sheetshow, setSheetshow] = useState(false);
   const [selectedsheet, setSelectedsheet] = useState([]);
@@ -161,10 +208,15 @@ export default function Sheets() {
   const [sheets, setSheets] = useState([]);
   const [fileshow, setFileshow] = useState(false);
   const [userid, setUserid] = useState("");
+  const [reload, setReload] = useState(false);
+  const [sheet_id, setSheet_id] = useState("");
+  const [sheets_id, setSheets_id] = useState("");
+
   useEffect(() => {
     const getSheets = async () => {
       try {
         if (!user) return console.log("User ID not available");
+        if (reload) setReload(false);
         const user_id = user._id;
         setUserid(user_id);
         const response = await axios.get(
@@ -174,12 +226,14 @@ export default function Sheets() {
           }
         );
         setSheets(response.data.sheets);
+        console.log("sheets_id ", response.data._id);
+        setSheets_id(response.data._id);
       } catch (error) {
         console.log(error);
       }
     };
     getSheets();
-  }, [user]);
+  }, [user, reload]);
   return (
     <div className="overflow-auto">
       {!sheetshow ? (
@@ -201,7 +255,7 @@ export default function Sheets() {
                 onClick={() => {
                   setSelectedsheet(it.sheet);
                   setSheetshow(true);
-                  console.log(selectedsheet);
+                  setSheet_id(it._id);
                 }}
                 className="hover:cursor-pointer hover:scale-105 duration-300 p-5 rounded-xl h-44 flex border flex-col bg text-2xl text-black font-bold bg-white"
               >
@@ -217,10 +271,20 @@ export default function Sheets() {
           </div>
         </div>
       ) : (
-        <Sheet setSheetshow={setSheetshow} selectedsheet={selectedsheet} />
+        <Sheet
+          setSheetshow={setSheetshow}
+          selectedsheet={selectedsheet}
+          sheet_id={sheet_id}
+          sheets_id={sheets_id}
+          setReload={setReload}
+        />
       )}
       {fileshow && (
-        <FileUploadForm setFileshow={setFileshow} user_id={userid} />
+        <FileUploadForm
+          setFileshow={setFileshow}
+          user_id={userid}
+          setReload={setReload}
+        />
       )}
     </div>
   );
